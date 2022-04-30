bec5a0199d423ab4b9ffec4b9b016a3376dd60c1
viraltaco <vt_@gmx.us>
2022-04-30 13:22:39 +0200

    vt::remove_cvref, vt::is_nullptr

--- src/internal/type_traits.hxx
+++ src/internal/type_traits.hxx
@@ -1,13 +1,7 @@
 #pragma once /// copyright 2022 viraltaco_ <https://opensource.org/licenses/MIT>
-#ifndef com_viraltaco_type_traits_hxx
-#define com_viraltaco_type_traits_hxx
-static_assert (
-  __cplusplus > 201700L,
-  "Outdated C++ standard version. Please use C++20 and above."
-);
+static_assert ( __cplusplus >= 2020'02, "C++20 is required by: " __FILE__ );
 
 namespace vt::inline detail {
-
 template <class T, T v> struct integral_constant {
   using value_type = decltype (v);
   using type = integral_constant<T, v>;
@@ -16,8 +10,8 @@ template <class T, T v> struct integral_constant {
   explicit constexpr operator value_type() const noexcept { return v; }
 };
 
-using false_type = typename integral_constant<bool, false>::type;
-using true_type  = typename integral_constant<bool, true>::type;
+using false_type = typename vt::integral_constant<bool, false>::type;
+using true_type  = typename vt::integral_constant<bool, true>::type;
 
 template <class T, class U> struct is_same       : false_type {};
 template <class T>          struct is_same<T, T> : true_type  {};
@@ -36,7 +30,14 @@ template <class T> struct remove_ref<T&>  { using type = T; };
 template <class T> struct remove_ref<T&&> { using type = T; };
 template <class T>
 using remove_ref_t = typename vt::remove_ref<T>::type;
+
+template <class T>
+struct remove_cvref { using type = vt::remove_cv_t<vt::remove_ref_t<T>>; };
+template <class T>
+using remove_cvref_t = typename vt::remove_cvref<T>::type;
+
+template <class T>
+struct is_nullptr : vt::is_same<vt::remove_cv_t<T>, decltype (nullptr)> {};
+template <class T>
+static constexpr auto is_nullptr_v = vt::is_nullptr<T>::value;
 } // namespace vt::inline detail
-
-
-#endif // com_viraltaco_type_traits_hxx
