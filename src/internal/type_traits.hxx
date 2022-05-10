0e1af1e7e4505419095d92b5b6536012b378e635
viraltaco <vt_@gmx.us>
2022-05-11 00:58:04 +0200

    Formatted code.

--- src/internal/type_traits.hxx
+++ src/internal/type_traits.hxx
@@ -1,7 +1,9 @@
 #pragma once /// copyright 2022 viraltaco_ <https://opensource.org/licenses/MIT>
 
-namespace vt::inline detail {
-template <class T, T v> struct integral_constant {
+namespace vt::
+inline detail {
+template <class T, T v>
+struct integral_constant {
   using value_type = T;
   using type = integral_constant<T, v>;
   static constexpr value_type value = v;
@@ -10,23 +12,32 @@ template <class T, T v> struct integral_constant {
 };
 
 using false_type = typename vt::integral_constant<bool, false>::type;
-using true_type  = typename vt::integral_constant<bool, true>::type;
+using true_type = typename vt::integral_constant<bool, true>::type;
 
-template <class T, class U> struct is_same       : false_type {};
-template <class T>          struct is_same<T, T> : true_type  {};
+template <class T, class U>
+struct is_same : false_type {};
+template <class T>
+struct is_same<T, T> : true_type {};
 template <class T, class U>
 static constexpr auto is_same_v = vt::is_same<T, U>::value;
 
-template <class T> struct remove_cv                   { using type = T; };
-template <class T> struct remove_cv<const T>          { using type = T; };
-template <class T> struct remove_cv<volatile T>       { using type = T; };
-template <class T> struct remove_cv<const volatile T> { using type = T; };
+template <class T>
+struct remove_cv { using type = T; };
+template <class T>
+struct remove_cv<const T> { using type = T; };
+template <class T>
+struct remove_cv<volatile T> { using type = T; };
+template <class T>
+struct remove_cv<const volatile T> { using type = T; };
 template <class T>
 using remove_cv_t = typename vt::remove_cv<T>::type;
 
-template <class T> struct remove_ref      { using type = T; };
-template <class T> struct remove_ref<T&>  { using type = T; };
-template <class T> struct remove_ref<T&&> { using type = T; };
+template <class T>
+struct remove_ref { using type = T; };
+template <class T>
+struct remove_ref<T &> { using type = T; };
+template <class T>
+struct remove_ref<T &&> { using type = T; };
 template <class T>
 using remove_ref_t = typename vt::remove_ref<T>::type;
 
@@ -36,7 +47,7 @@ template <class T>
 using remove_cvref_t = typename vt::remove_cvref<T>::type;
 
 template <class T>
-struct is_nullptr : vt::is_same<vt::remove_cv_t<T>, decltype (nullptr)> {};
+struct is_nullptr : vt::is_same<vt::remove_cv_t<T>, decltype(nullptr)> {};
 template <class T>
 static constexpr auto is_nullptr_v = vt::is_nullptr<T>::value;
 } // namespace vt::inline detail
--- src/internal/utility.hxx
+++ src/internal/utility.hxx
@@ -1,25 +1,28 @@
 #pragma once /// copyright 2022 viraltaco_ <https://opensource.org/licenses/MIT>
 #include "type_traits.hxx"
 
-namespace vt::inline detail {
- template <auto... Is> struct integer_sequence {
-   static constexpr auto size() noexcept { return sizeof... (Is); }
- };
+namespace vt::
+inline detail {
+template <auto... Is>
+struct integer_sequence {
+  static constexpr auto size() noexcept { return sizeof... (Is); }
+};
 
- template <class T, class U = vt::remove_ref_t<T>> [[nodiscard]]
- constexpr U&& move(T&& o) noexcept { return static_cast<U&&> (o); }
+template <class T, class U = vt::remove_ref_t<T>>
+[[nodiscard]]
+constexpr U &&move(T &&o) noexcept { return static_cast<U &&> (o); }
 
- template <class T>
- constexpr void swap(T& a, T& b) noexcept {
-   T _a = vt::move(a);
-   a = vt::move(b);
-   b = vt::move(_a);
- }
+template <class T>
+constexpr void swap(T &a, T &b) noexcept {
+  T _a = vt::move(a);
+  a = vt::move(b);
+  b = vt::move(_a);
+}
 
- template <class FwL, class FwR>
- constexpr FwR swap_ranges(FwL l, FwL last, FwR r) noexcept {
-   for (; l != last; ++l, static_cast<void> (++r))
-     vt::swap(*l, *r);
-   return r;
- }
+template <class FwL, class FwR>
+constexpr FwR swap_ranges(FwL l, FwL last, FwR r) noexcept {
+  for (; l != last; ++l, static_cast<void> (++r))
+    vt::swap(*l, *r);
+  return r;
+}
 } // namespace vt::inline::detail
