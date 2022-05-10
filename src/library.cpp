d9e4c6fa414fa97bc527c2bb1286f441cb925ebf
viraltaco <vt_@gmx.us>
2022-05-11 00:57:44 +0200

    Implemented 'vector' (Norm 1 tensor) by scalar operations (similar to `std::valarray` Formated code using the previously added `.clang-format`. Style is based on Google. No additional options as of now.

--- src/library.cpp
+++ src/library.cpp
@@ -9,7 +9,10 @@ namespace vt::info {
 
 int main() {
   vt::array<int, 5> a = { 1, 2, 3, 4, 5, };
-  std::cout << a;
-  auto b = vt::array<char, sizeof "Hello, World!">{ "Hello, World!" };
-  std::cout.put('\n') << b;
+  std::cout << "a: " << a;
+  auto b = vt::array{ "Hello, World!" };
+  std::cout.put('\n') << "b: " << b << "\tsize: " << b.size();
+  std::cout.put('\n') << "b == b: " << std::boolalpha << (b == b);
+  std::cout.put('\n') << "a + 5: " << (a + 5);
+  std::cout.put('\n') << "a & 1: " << (a & 1);
 }
