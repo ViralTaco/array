2b111f3f7f01b3c7c4184349fcc26a4a3221d607
viraltaco <vt_@gmx.us>
2022-08-08 11:22:27 +0200

    Adjusted version string to v0.2.0

--- src/library.cpp
+++ src/library.cpp
@@ -2,7 +2,7 @@
 #include "array.hxx"
 
 namespace vt::info {
- static constexpr auto version = "com.viraltaco.array v0.1.0",
+  static constexpr auto version = "com.viraltaco.array v0.2.0",
     copyright = "Copyright 2022 viraltaco_ <https://viraltaco.com/array>\n",
     license = "MIT License. See: <https://opensource.org/licenses/MIT>\n";
  } // namespace vt::info
@@ -10,7 +10,7 @@ namespace vt::info {
 int main() {
   vt::array<int, 5> a = { 1, 2, 3, 4, 5, };
   std::cout << "a: " << a;
-  auto b = vt::array{ "Hello, World!" };
+  auto b = vt::array{ "Hello, World!"  };
   std::cout.put('\n') << "b: " << b << "\tsize: " << b.size();
   std::cout.put('\n') << "b == b: " << std::boolalpha << (b == b);
   std::cout.put('\n') << "a + 5: " << (a + 5);
